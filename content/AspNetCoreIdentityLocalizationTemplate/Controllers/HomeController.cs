2658327433e55613a9d0dab5f550c7aba9a82efd
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2020-06-10 12:15:12 +0300

    ...

--- content/AspNetCoreIdentityLocalizationTemplate/Controllers/HomeController.cs
+++ content/AspNetCoreIdentityLocalizationTemplate/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AspNetCoreIdentityLocalization.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+
 
 namespace AspNetCoreIdentityLocalization.Controllers
 {
@@ -32,6 +32,17 @@ namespace AspNetCoreIdentityLocalization.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1), Secure = true, SameSite = SameSiteMode.None }
+            );
+
+            return LocalRedirect(returnUrl);
         }
     }
 }
